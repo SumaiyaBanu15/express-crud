9b111f5501a0cf394d3b81a26a2508d06b0a636e
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2023-10-16 12:06:27 +0530

    Express-CRUD

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express') //commonJS Import older version
+// import express from 'express'//ES Module Import add "type": "module" in package.json
+
+const dotenv = require('dotenv')
+const AppRoutes = require('./src/Routes')
+
+dotenv.config()
+const PORT  = process.env.PORT
+
+const app = express()
+app.use(express.json())
+
+app.use('/',AppRoutes)
+
+
+app.listen(PORT,()=>console.log(`Server listening to port ${PORT}`))
---
+++ src/models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+try {
+    console.log(process.env.dbUrl,process.env.dbName,'\n')
+    mongoose.connect(`${process.env.dbUrl}/${process.env.dbName}`)
+} catch (error) {
+    console.log(error)
+}
+
+module.exports = mongoose
